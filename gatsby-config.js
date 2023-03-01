7a93aaedbb3a8558e850cfac5966ef37d1f05589
midhubalan <midhubalan@outlook.com>
2023-02-28 19:43:17 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `teachingbusiness`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
